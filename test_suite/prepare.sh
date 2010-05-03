f60c4107c02a04ae3191c5b6395a6daf275ee2b9
m00dawg <tim@holland.dev>
2010-05-03 15:06:57 -0200

    Removed old test suite

--- test_suite/prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Copying backup-sets to /etc/holland/backupsets"
-cp -a backupsets/*conf /etc/holland/backupsets/
--- test_suite/test_backups.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Running mk-table-checksum for a Bassline"
-mk-table-checksum --checksum localhost > mk-checksum-before
-
-#source mysqldump-table-filtering.sh
-source mysql-lvm-recovery.sh
-
