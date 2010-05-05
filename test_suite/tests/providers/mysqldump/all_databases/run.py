a2ea35074a52688afbf0c9a5a9036bd761830936
m00dawg <tim@holland.dev>
2010-05-05 05:10:15 -0200

    all-databases test now can install Holland

--- test_suite/tests/providers/mysqldump/all_databases/run.py
+++ test_suite/tests/providers/mysqldump/all_databases/run.py
@@ -3,14 +3,25 @@ import shlex, subprocess
 import shutil
 import ConfigParser
 
-pwd = os.getcwd()
+# Functions
+def execute(config, cmd):
+    returnCode = subprocess.call(cmd, 
+        stdout=config.get('global', 'output_log'), 
+        stderr=config.get('global', 'error_log'))
+    if returnCode != 0:
+        print "Failure is not an option! Except when it is"
+        sys.exit(returnCode)
+    return
 
+# Setup
+pwd = os.getcwd()
 config = ConfigParser.ConfigParser()
 config.read('test.conf')
 
 # Create Sandbox
-sandboxLog = open('results/sandbox.log', 'w')
-proc = subprocess.Popen([
+outputLog = open('results/output.log', 'w')
+errorLog = open('results/error.log', 'w')
+execute([
     'make_sandbox', 
     config.get('sandbox','tarball'),
     '--upper_directory=' + config.get('sandbox','upper_directory'),
@@ -20,9 +31,22 @@ proc = subprocess.Popen([
     '--db_user=' + config.get('sandbox', 'user'),
     '--db_password=' + config.get('sandbox', 'password'),
     '--no_confirm'
-], 0, None, sandboxLog, sandboxLog)
-proc.communicate()
+])
+"""
+# Setup Holland virtual environment
+if not(os.path.exists(config.get('global', 'holland_install_dir'))):
+    os.mkdir(config.get('global', 'holland_install_dir'))
+path = pwd + '/' + config.get('global', 'holland_install_dir')
+status = subprocess.call(['virtualenv', path]) 
+os.environ['PATH'] = os.path.join(path, 'bin') + ':' + os.environ['PATH']
+subprocess.call(['python', 'setup.py', 'install'],
+    cwd=config.get('global', 'holland_repository'))
+
+for plugin in open(config.get('global', 'holland_repository') + '/plugins/ACTIVE', 'r'):
+    print plugin
 
 # Cleanup
-subprocess.Popen([pwd + '/sandbox/mysql/stop'], shell=True)
+subprocess.call([pwd + '/sandbox/mysql/stop'], shell=True)
 shutil.rmtree(pwd + '/sandbox/mysql')
+shutil.rmtree(pwd + '/' + config.get('global', 'holland_install_dir')
+"""
