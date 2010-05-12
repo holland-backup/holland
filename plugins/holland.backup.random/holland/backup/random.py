2234f5f77c54b092d8a3633b368b3171510fb730
yodermk <micah.yoder@rackspace.com>
2010-05-12 09:13:55 -0500

    Add the random plugin

---
+++ plugins/holland.backup.random/holland/backup/random.py
@@ -0,0 +1,42 @@
+import logging
+import os
+LOG = logging.getLogger(__name__)
+
+CONFIGSPEC="""
+[random]
+bytes = integer(default=50)
+""".splitlines()
+
+class RandomPlugin(object):
+    """Back up randomness"""
+
+    def __init__(self, name, config, target_directory, dry_run=False):
+        """Create new RandomPlugin instance"""
+
+        self.name = name
+        self.config = config
+        self.target_directory = target_directory
+        self.dry_run = dry_run
+        LOG.info("Validating Config")
+        self.config.validate_config(CONFIGSPEC)
+        self.bytes = self.config['random']['bytes']
+
+    def estimate_backup_size(self):
+        return self.bytes
+
+    def backup(self):
+        rand = open("/dev/random", "r")
+        bytesleft = self.bytes
+        data = ''
+        while bytesleft > 0:
+            r = rand.read(bytesleft)
+            data += r
+            bytesleft -= len(r)
+            LOG.info("Read %d bytes from /dev/random" % len(r))
+
+        outfile = os.path.join(self.target_directory, 'random_data')
+	f = open(outfile, "w")
+	f.write(data)
+        f.close()
+        LOG.info("Wrote to "+outfile)
+
---
+++ plugins/holland.backup.random/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = '0.9.9'
+
+setup(name='holland.backup.random',
+      version=version,
+      description=" Back up random useless crap!",
+      long_description="""\
+      Uses /dev/random. A bit more of an example then holland.backup.example
+      """,
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='random',
+      author='Rackspace',
+      author_email='holland-devel@googlegroups.com',
+      url='http://www.hollandbackup.org/',
+      license='GPLv2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests',
+                                      'tests.*']),
+      include_package_data=True,
+      zip_safe=True,
+      test_suite='tests',
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [holland.backup]
+      random = holland.backup.random:RandomPlugin
+      """,
+      namespace_packages=['holland', 'holland.backup'],
+    )
