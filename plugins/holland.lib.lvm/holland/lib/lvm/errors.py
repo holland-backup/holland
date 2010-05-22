d7fd78831da61628031456a75392bda2191311ea
Andrew Garner <muzazzi@gmail.com>
2010-05-22 03:55:01 -0400

    add missing errors.py

---
+++ plugins/holland.lib.lvm/holland/lib/lvm/errors.py
@@ -0,0 +1,19 @@
+"""Standard errors raised by holland.lib.lvm"""
+
+class SnapshotError(Exception):
+    """Error occurred during snapshotting process"""
+
+class LVMCommandError(Exception):
+    """Error occurred while running a lvm command
+    
+    :attribute cmd: The command that was being run
+    :attribute status: exit status of the command
+    :attribute error: stderr output of the command
+    """
+
+    def __init__(self, cmd, status, error):
+        error = error.strip()
+        Exception.__init__(self, cmd, status, error)
+        self.cmd = cmd
+        self.status = status
+        self.error = error
