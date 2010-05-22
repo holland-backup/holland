63d5d88ba0e4fc8ee6836f6065314043789ea792
Andrew Garner <muzazzi@gmail.com>
2010-05-22 03:31:13 -0400

    cleanup and simplify snapshot implementation

--- plugins/holland.lib.lvm/holland/lib/lvm/snapshot.py
+++ plugins/holland.lib.lvm/holland/lib/lvm/snapshot.py
@@ -1,226 +1,97 @@
-"""LVM Snapshot Lifecycle """
+"""LVM Snapshot state machine"""
 
-import os
-import sys
-import shutil
 import logging
-import tempfile
-from holland.lib.lvm.base import LogicalVolume
-from holland.lib.lvm.util import getmount, relpath, format_bytes
-
-__all__ = [
-    'SnapshotLifecycle'
-]
+from holland.lib.lvm import LogicalVolume
 
 LOG = logging.getLogger(__name__)
 
-class LVMError(Exception):
-    """LVMError"""
+__all__ = [
+    'Snapshot',
+    'CallbackFailuresError',
+]
 
-class SnapshotLifecycle(object):
-    """A simple state machine to run through the LVM snapshot lifecycle"""
-    tempdir = False
+class Snapshot(object):
+    """Snapshot state machine"""
+    def __init__(self, name, size, mountpoint):
+        self.name = name
+        self.size = size
+        self.mountpoint = mountpoint
+        self.callbacks = {}
 
-    def __init__(self,
-                 target_directory,
-                 snapshot_mountpoint=None,
-                 snapshot_name=None,
-                 snapshot_size=None):
-        # track the target directory
-        self.target_directory = os.path.realpath(target_directory)
-        # logical_volume = handle to logical volume we're snapshotting
-        self.logical_volume = None
+    def start(self, path):
+        """Start the snapshot process to snapshot the logical volume
+        that ``path`` exists on.
 
-        self.snapshot_mountpoint = snapshot_mountpoint
-        if not self.snapshot_mountpoint:
-            self.snapshot_mountpoint = tempfile.mkdtemp()
-            self.tempdir = True
-
-        self.snapshot_name = snapshot_name
-        self.snapshot_size = snapshot_size
-        # Placeholder for snapshot handle
-        self.snapshot = None
-
-    def start(self):
-        """Start and initialize the snapshot process"""
-        logging.info("Finding volume for directory: %s",
-                     self.target_directory)
-        mountpoint = getmount(self.target_directory)
-        LOG.info("Mount point for directory: %s", mountpoint)
-        self.logical_volume = LogicalVolume.find_mounted(mountpoint)
-        if not self.logical_volume:
-            raise LVMError("No logical volume found for "
-                           "mountpoint=%r target_path=%r" % 
-                           (mountpoint, self.target_directory))
-        LOG.info("%r is on logical volume %s/%s",
-                        self.target_directory,
-                        self.logical_volume.vg_name,
-                        self.logical_volume.lv_name)
-
-        self.run_callback('init')
-        return self.create_snapshot()
-
-    def create_snapshot(self):
-        """Snapshot the target LVM volume
-
-        Before the snapshot is taken, a 'presnapshot'
-        callback will be run.
-
-        After the snapshot is taken, a postsnapshot
-        callback will be run.
         """
-
-        LOG.info("Running pre-snapshot tasks")
-        self.run_callback('presnapshot')
-
         try:
-            self.snapshot = self.logical_volume.snapshot(self.snapshot_name,
-                                                         self.snapshot_size)
-            LOG.info("Created snapshot /dev/%s/%s [%s] from logical volume"
-                        " /dev/%s/%s",
-                        self.snapshot.vg_name,
-                        self.snapshot.lv_name,
-                        format_bytes(int(self.snapshot.lv_size)),
-                        self.logical_volume.vg_name,
-                        self.logical_volume.lv_name)
+            logical_volume = LogicalVolume.lookup_from_path(path)
+        except Exception:
+            return self.error(None)
+        return self.create_snapshot(logical_volume)
 
-        except EnvironmentError, exc: # covers LVMError,OSError, etc.
-            # Log error
-            # go to 'remove' state
-            LOG.error("Failed to snapshot %s/%s: %s",
-                         self.logical_volume.vg_name,
-                         self.logical_volume.lv_name,
-                         exc)
-            return self.cleanup()
-        LOG.info("Running post-snapshot tasks")
-        self.run_callback('postsnapshot')
-        return self.mount()
+    def create_snapshot(self, logical_volume):
+        """Create a snapshot for the given logical volume
 
-    def mount(self):
-        """Mount the snapshotted LVM volume"""
-
-        LOG.info("Running pre-mount tasks")
-        self.run_callback('premount')
-        # This may fail (OSError -> no such mountpoint, etc.)
-        try:
-            self.snapshot.mount(self.snapshot_mountpoint)
-        except EnvironmentError, exc:
-            LOG.error("Failed to mount %s/%s on %s: %s",
-                         self.snapshot.vg_name,
-                         self.snapshot.lv_name,
-                         self.snapshot_mountpoint,
-                         exc)
-            return self.remove()
-        return self.backup()
-
-    def backup(self):
-        """Backup the snapshotted LVM volume"""
-        LOG.info("Running backup tasks")
-        rpath = relpath(getmount(self.target_directory),
-                        self.target_directory)
-        source_directory = os.path.join(self.snapshot_mountpoint,
-                                        rpath)
-        self.run_callback('backup', os.path.realpath(source_directory))
-        return self.unmount()
-
-    def unmount(self):
-        """Unmount the snapshotted LVM volume"""
-        try:
-            self.snapshot.unmount()
-            LOG.info("Unmounted %s/%s",
-                        self.snapshot.vg_name,
-                        self.snapshot.lv_name)
-        except EnvironmentError, exc:
-            LOG.error("Failed to unmount snapshot: %s", exc)
-            return self.cleanup()
-        return self.remove()
-
-    def remove(self):
-        """Remove the LVM snapshot"""
-        self.run_callback('preremove', self.snapshot)
-        try:
-            self.snapshot.refresh()
-            total_snapshot_space = int(self.snapshot.lv_size)
-            snap_percent_used = float(self.snapshot.snap_percent)
-            used_snapshot_space = total_snapshot_space*(snap_percent_used/100.0)
-            LOG.info("Final snapshot size %s [%.2f%% of %s]",
-                format_bytes(used_snapshot_space),
-                snap_percent_used,
-                format_bytes(total_snapshot_space)
-            )
-        except LVMError, exc:
-            LOG.info("Failed to refresh snapshot: %s", exc)
-
-        try:
-            self.snapshot.remove()
-            LOG.info("Removed snapshot %s/%s",
-                        self.snapshot.vg_name,
-                        self.snapshot.lv_name)
-            self.snapshot = None
-        except AssertionError, exc:
-            LOG.error("Failed to remove snapshot: %s", exc)
-
-        return self.cleanup()
-
-
-    def _check_for_existing_snapshot(self):
-        """Check if an existing snapshot is alive"""
-        if not self.snapshot and self.logical_volume:
-            snapshot_lv = '%s/%s' % (self.logical_volume.vg_name,
-                                     self.snapshot_name or
-                                     self.logical_volume.lv_name + '_snapshot')
-            LOG.info("Checking for outstanding snapshot: %s", snapshot_lv)
-            try:
-                self.snapshot = LogicalVolume.find_one(snapshot_lv)
-            except ValueError:
-                LOG.info("No outstanding snapshot found.")
-
-    def cleanup(self):
-        """Cleanup as much as possible in case of an error
-
-        If this method is called in the context of a raised exception,
-        that exception will be raised at the end of the cleanup.
         """
-
-        self._check_for_existing_snapshot()
-
-        if self.snapshot and self.snapshot.is_mounted():
-            try:
-                self.snapshot.unmount()
-                logging.info("[cleanup] Unmounted snapshot %s", self.snapshot)
-            except (KeyboardInterrupt, SystemExit):
-                # These errors should be handled, and balanced with
-                # cleanup
-                pass
-            except (LVMError, OSError), exc:
-                logging.error("Failed to unmount snapshot on cleanup: %s", exc)
-
-        if self.snapshot and self.snapshot.exists():
-            try:
-                self.snapshot.remove()
-                logging.info("[cleanup] Removed snapshot %s", self.snapshot)
-            except (LVMError, OSError), exc:
-                logging.error("Failed to remove snapshot on cleanup: %s", exc)
-
-        if self.tempdir and os.path.exists(self.snapshot_mountpoint):
-            try:
-                shutil.rmtree(self.snapshot_mountpoint)
-                logging.info("[cleanup] removed temporary snapshot "
-                             "mountpoint %s",
-                             self.snapshot_mountpoint)
-            except IOError, exc:
-                LOG.error("Failed to remove tempfile generated snapshot "
-                             "mountpoint: %s", exc)
-        # reraise any exceptions that propagated to us
-        if sys.exc_info() != (None, None, None):
-            LOG.debug("Reraising exception", exc_info=True)
-            raise
-
-
-    def run(self):
-        """Run through the lifecycle of an LVM snapshot backup"""
         try:
-            self.start()
-        except:
-            self.cleanup()
-            raise
+            snapshot = logical_volume.snapshot(self.name, self.size)
+        except Exception:
+            return self.error(None)
+
+        return self.mount_snapshot(snapshot)
+
+    def mount_snapshot(self, snapshot):
+        """Mount the snapshot"""
+        try:
+            snapshot.mount(self.mountpoint)
+        except Exception:
+            return self.error(snapshot)
+
+        return self.unmount_snapshot(snapshot)
+
+    def unmount_snapshot(self, snapshot):
+        """Unmount the snapshot"""
+        try:
+            snapshot.unmount()
+        except Exception:
+            return self.error(snapshot)
+
+        return self.remove_snapshot(snapshot)
+
+    def remove_snapshot(self, snapshot):
+        """Remove the snapshot"""
+        try:
+            snapshot.remove()
+        except Exception:
+            return self.error(snapshot)
+
+        return self.finish()
+
+    def finish(self):
+        """Finish the snapshotting process"""
+        pass
+
+    def register(self, event, callback, priority=100):
+        """Register a callback for ``event`` with ``priority``
+
+        """
+        self.callbacks.setdefault(event, []).append((priority, callback))
+
+    def _apply_callbacks(self, event, *args, **kwargs):
+        """Apply callbacks for event"""
+        callback_list = list(self.callbacks.get(event, []))
+        callback_list.sort(reverse=True)
+        callback_list = [callback[1] for callback in callback_list]
+        errors = []
+        for callback in callback_list:
+            try:
+                callback(event, *args, **kwargs)
+            except Exception, exc:
+                errors.append((callback, exc))
+
+        if errors:
+            raise CallbackFailuresError(errors)
+
+class CallbackFailuresError(Exception):
+    """Error running callbacks"""
+    pass
