1bb778793fcc610ef5b39978eda9fcd48399b0ee
Andrew Garner <muzazzi@gmail.com>
2010-05-24 13:32:55 -0400

    export ConfigError in holland.core.config

--- holland/core/config/__init__.py
+++ holland/core/config/__init__.py
@@ -1,4 +1,5 @@
-from config import hollandcfg, setup_config, load_backupset_config, BaseConfig
+from config import hollandcfg, setup_config, load_backupset_config, \
+                   BaseConfig, ConfigError
 from configobj import ConfigObj, ParseError, ConfigObjError
 
 __all__ = [
