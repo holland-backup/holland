c8ef96f77745c801d6254a82c23cea528d2d78fe
Andrew Garner <muzazzi@gmail.com>
2010-05-25 16:46:52 -0400

    start basic tests for holland.lib.lvm.base

--- plugins/holland.lib.lvm/tests/test_base.py
+++ plugins/holland.lib.lvm/tests/test_base.py
@@ -1,1 +1,21 @@
+from nose.tools import *
 from holland.lib.lvm.base import *
+
+def test_basevolume():
+    assert_raises(NotImplementedError, Volume)
+
+    class Test(Volume):
+        pass
+
+    ok_(Test())
+
+    volume = Test({ 'foo' : 'bar', 'baz' : 'biz' })
+    assert_equals(volume.foo, 'bar')
+    assert_equals(volume.baz, 'biz')
+    assert_raises(AttributeError, volume.__getattr__, 'blah')
+
+    assert_raises(NotImplementedError, volume.reload)
+    assert_raises(NotImplementedError, Test.lookup, 'foo')
+    assert_raises(NotImplementedError, Test.search, 'foo')
+
+    assert_equals(repr(Test()), 'Test()')
