851a691db0d377490afe2b4084df2380a380107a
Andrew Garner <muzazzi@gmail.com>
2010-05-26 11:54:52 -0400

    add LogicalVolume tests

--- plugins/holland.lib.lvm/tests/test_base.py
+++ plugins/holland.lib.lvm/tests/test_base.py
@@ -72,3 +72,51 @@ class TestPhysicalVolume(object):
     def test_repr(self):
         pv = PhysicalVolume.lookup('/dev/foo1')
         assert_equals(repr(pv), "PhysicalVolume(device='/dev/foo1')")
+
+
+class TestLogicalVolume(object):
+    mocker = Mocker()
+
+    def setup_class(cls):
+        lvs = cls.mocker.replace('holland.lib.lvm.raw.lvs')
+        lvs(MATCH(lambda pathspec: pathspec.endswith('dba/mysql') or \
+                                   pathspec.endswith('mappper/dba-mysql')))
+        cls.mocker.count(min=3)
+        cls.mocker.result([{ 'lv_name' : 'mysql', 'vg_name' : 'dba' }])
+        # mock blkid too
+        blkid = cls.mocker.replace('holland.lib.lvm.raw.blkid')
+        blkid('/dev/dba/mysql')
+        cls.mocker.result([{ 'type' : 'ext3' }])
+        blkid(MATCH(lambda arg: arg != '/dev/dba/mysql'))
+        cls.mocker.result([])
+        cls.mocker.replay()
+    setup_class = classmethod(setup_class)
+
+    def teardown_class(cls):
+        cls.mocker.restore()
+    teardown_class = classmethod(teardown_class)
+
+    def test_create(self):
+        """Test creating a LogicalVolume"""
+        lv = LogicalVolume({ 'lv_name' : 'mysql', 'vg_name' : 'dba' })
+        assert_equals(lv.lv_name, 'mysql')
+
+    def test_happy_lookup(self):
+        """Test a loading an existing lv"""
+        lv = LogicalVolume.lookup('dba/mysql')
+
+    def test_filesystem(self):
+        """Test looking up filesystem of lv"""
+        lv = LogicalVolume.lookup('dba/mysql')
+        assert_equals(lv.filesystem(), 'ext3')
+
+    def test_bad_filesystem(self):
+        """Test looking for filesystem of a lv that doesn't exist"""
+        lv = LogicalVolume()
+        lv.vg_name = 'dba'
+        lv.lv_name = 'mysql_snapshot' # <- doesn't exist in our mock
+        assert_raises(ValueError, lv.filesystem)
+
+    def test_repr(self):
+        lv = LogicalVolume.lookup('dba/mysql')
+        assert_equals(repr(lv), 'LogicalVolume(device=\'/dev/dba/mysql\')')
