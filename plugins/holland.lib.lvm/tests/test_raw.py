9c92d65a57de1b5b3ad4b77c183a7924b53dc9ad
Andrew Garner <muzazzi@gmail.com>
2010-05-26 23:22:51 -0400

    move lvm boostrapping code to tests package fixture

--- plugins/holland.lib.lvm/tests/test_raw.py
+++ plugins/holland.lib.lvm/tests/test_raw.py
@@ -4,47 +4,7 @@ import tempfile
 import subprocess
 from nose.tools import *
 from holland.lib.lvm.raw import *
-
-LOOP_DEV = '/dev/loop0'
-IMG_FILE = '/tmp/hl_lvm.img'
-IMG_SIZE = 128*1024**2
-TEST_VG = 'holland'
-TEST_LV = 'test_lv'
-MNT_DIR = tempfile.mkdtemp()
-
-def setup():
-    """Setup a simple LVM device to use"""
-    os.environ['PATH'] = '/sbin:/usr/sbin:' + os.environ['PATH']
-    size = IMG_SIZE / 512
-    img_path = os.path.join(MNT_DIR, 'test.img')
-    subprocess.call("dd if=/dev/zero of=%s count=%d" % 
-                    (img_path, size), shell=True)
-    subprocess.call("losetup %s %s" % (LOOP_DEV, img_path), shell=True)
-    subprocess.call("pvcreate %s" % LOOP_DEV, shell=True)
-    subprocess.call("vgcreate %s %s" % 
-                    (TEST_VG, LOOP_DEV), shell=True)
-    subprocess.call("lvcreate -L%dK -n %s %s" %
-                    ((IMG_SIZE / 2) / 1024, TEST_LV, TEST_VG), shell=True)
-    subprocess.call("mkfs.ext3 /dev/%s/%s" % 
-                    (TEST_VG, TEST_LV), shell=True)
-    subprocess.call("mount /dev/%s/%s %s" %
-                    (TEST_VG, TEST_LV, MNT_DIR), shell=True)
-    # dd if=/dev/zero of=$staging/foo.img count=N
-    # losetup /dev/loopN $staging/foo.img
-    # pvcreate /dev/loopN
-    # vgcreate $test_vg /dev/loopN
-    # lvcreate $test_lv
-    # mkfs /dev/$test_vg/$test_lv 
-    # mount /dev/$test_vg/$test_lv somepath
-
-
-def teardown():
-    """Remove the previously setup LVM"""
-    subprocess.call("umount %s" % MNT_DIR, shell=True)
-    subprocess.call("vgremove -f %s" % TEST_VG, shell=True)
-    subprocess.call('losetup -d %s' % LOOP_DEV, shell=True)
-    shutil.rmtree(MNT_DIR)
-
+from tests.constants import *
 
 def test_pvs():
     pvs(LOOP_DEV)
@@ -70,12 +30,22 @@ def ensure_snapshot_unmount():
 
 def test_snapshot():
     # def lvsnapshot(orig_lv_path, snapshot_name, snapshot_extents, chunksize=None):
+    for info in vgs(TEST_VG):
+        print >>sys.stderr, info
+    for info in lvs(TEST_VG):
+        print >>sys.stderr, info
+
     lvsnapshot('%s/%s' % (TEST_VG, TEST_LV), '%s_snapshot' % TEST_LV , 4, '512K')
-    mount('/dev/%s/%s' % (TEST_VG, TEST_LV), '/mnt/tmp')
+    assert_raises(LVMCommandError, lvsnapshot, '%s/%s' % (TEST_VG, TEST_LV), '%s_snapshot' % TEST_LV , 1)
+    mount('/dev/%s/%s' % (TEST_VG, TEST_LV), '/mnt/tmp', options='noatime', vfstype='ext3')
     umount('/dev/%s/%s' % (TEST_VG, TEST_LV))
     lvremove('%s/%s_snapshot' % (TEST_VG, TEST_LV))
+    assert_raises(LVMCommandError, lvremove, '%s/%s_snapshot' % (TEST_VG, TEST_LV)) # this should fail the 2nd time
 test_snapshot.teardown = ensure_snapshot_unmount()
 
 def test_blkid():
     info, = blkid('/dev/%s/%s' % (TEST_VG, TEST_LV))
     assert_equals(info['type'], 'ext3')
+
+def test_bad_mount():
+    assert_raises(LVMCommandError, mount, '/dev/%s/%s' % (TEST_VG, TEST_LV), os.path.join(MNT_DIR, 'missing'))
