2d81c849dd0f1137701e81365c66a740bfbdc7ee
Andrew Garner <muzazzi@gmail.com>
2010-06-07 00:23:57 -0400

    removing bitrot from mysql-lvm

--- plugins/holland.backup.mysql_lvm/run_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-TMPDEV=$(df /tmp | tail -n +2 | head -n 1 | awk '{ print $1; }')
-VGNAME='vg_test' TMPDEV=$TMPDEV LVMPATH=/usr/sbin sudo -E python setup.py nosetests -v -v -v -l DEBUG -s < /dev/null
