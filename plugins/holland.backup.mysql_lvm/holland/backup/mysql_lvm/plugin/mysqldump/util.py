4da81f5c80211383bfd3c409974d5d88bbddceb9
Andrew Garner <muzazzi@gmail.com>
2010-06-29 16:46:46 -0400

    fix minor typo

--- plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/plugin/mysqldump/util.py
+++ plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/plugin/mysqldump/util.py
@@ -95,7 +95,7 @@ def setup_actions(snapshot, config, client, datadir, plugin):
     act = MySQLDumpDispatchAction(plugin, mysqld_config)
     snapshot.register('post-mount', act, priority=100)
 
-    errlog_src = os.path.join(snap_datadir, 'holland_lvm.log')
+    errlog_src = os.path.join(datadir, 'holland_lvm.log')
     errlog_dst = os.path.join(spooldir, 'holland_lvm.log')
     snapshot.register('pre-unmount',
                       lambda *args, **kwargs: shutil.copyfile(errlog_src,
