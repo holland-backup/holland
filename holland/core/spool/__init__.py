63e91d2ca024a9cc5105868974c08b006fbd39ff
Andrew Garner <muzazzi@gmail.com>
2011-01-07 03:55:21 +0000

    cleanup spool implementation

--- holland/core/spool/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Manage and rotate backups in a directory spool"""
-
-from holland.core.spool.manager import SpoolManager
-from holland.core.spool.error import SpoolError
--- holland/core/spool/error.py
+++
@@ -1,4 +0,0 @@
-"""Standard exceptions raised by holland.core.spool"""
-
-class SpoolError(Exception):
-    """Base error class that all spool errors derive from"""
