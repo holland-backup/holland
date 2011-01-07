71465055499a9d93ac4e59eb2b56da0686bbc119
Andrew Garner <muzazzi@gmail.com>
2011-01-07 12:41:39 +0000

    change plugin api backup to group,name interface

--- holland/core/plugin/manager.py
+++ holland/core/plugin/manager.py
@@ -9,37 +9,35 @@ LOG = logging.getLogger(__name__)
 class AbstractPluginManager(object):
     """Interface that PluginManager implementations should follow"""
 
-    def load(self, name):
+    def load(self, group, name):
         """Load a plugin for the given name"""
         raise NotImplementedError()
 
-    def iterate(self, name):
+    def iterate(self, group, name):
         """Iterate over plugins for the given name"""
         raise NotImplementedError()
 
 class EntrypointPluginManager(AbstractPluginManager):
     """Plugin manager that uses setuptools entrypoints"""
 
-    def load(self, name):
+    def load(self, group, name):
         """Load a plugin via a setuptools entrypoint for the given name
 
         Name must be in the format group.name
         """
-        group, name = name.rsplit('.', 1)
-
         for plugin in iter_entry_points(group, name):
             try:
                 return plugin.load()
             except Exception, exc:
+                LOG.exception("Exception when loading plugin")
                 raise PluginError(exc)
-        raise PluginNotFoundError("No plugin found for group=%r name=%r" %
-                                  (group, name))
+        raise PluginNotFoundError("No plugin found for %s.%s" % (group, name))
 
-    def iterate(self, name):
+    def iterate(self, group):
         """Iterate over an entrypoint group and yield the loaded entrypoint
         object
         """
-        for plugin in iter_entry_points(name):
+        for plugin in iter_entry_points(group):
             try:
                 yield plugin.load()
             except (SystemExit, KeyboardInterrupt):
