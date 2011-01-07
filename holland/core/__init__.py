b802785194369ef8b6a9425ab7782ac51c0db3d4
Andrew Garner <muzazzi@gmail.com>
2011-01-07 04:10:22 +0000

    fix spool exports

--- holland/core/__init__.py
+++ holland/core/__init__.py
@@ -1,6 +1,6 @@
 """Holland core API"""
 
 from holland.core.plugin import load_plugin, iterate_plugins
-from holland.core.spool import SpoolManager, SpoolError
+from holland.core.spool import BackupSpool, BackupStore, SpoolError
 from holland.core.backup import BackupManager, BackupPlugin, BackupJob, \
                                 BackupError
