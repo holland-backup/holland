7447f0caa6bf689bf5aa9c2623d6fdf71ba6c78b
Andrew Garner <muzazzi@gmail.com>
2011-01-07 04:03:48 +0000

    adjust backup cmd to accomodate core api changes

--- holland/cli/commands/builtin/cmd_backup.py
+++ holland/cli/commands/builtin/cmd_backup.py
@@ -1,6 +1,7 @@
 import os, sys
-from holland.core import BackupManager, SpoolManager
+from holland.core import BackupManager, BackupJob, BackupError, BackupSpool
 from holland.cli.commands.base import ArgparseCommand, argument
+from holland.cli.config import load_backup_config
 
 class Backup(ArgparseCommand):
     name = 'backup'
@@ -15,25 +16,40 @@ class Backup(ArgparseCommand):
     ]
 
     def execute(self, namespace):
-        spool = SpoolManager(self.config['holland']['backup-directory'])
-        backup_mgr = BackupManager(spool)
-        base_path = os.path.dirname(self.config.filename)
+        spool = BackupSpool(self.config['holland']['backup-directory'])
+        backupmgr = BackupManager()
+        if not self.config.filename:
+            base_path = os.getcwd()
+        else:
+            base_path = os.path.dirname(self.config.filename)
 
         if namespace.dry_run:
-            run_backup = backup_mgr.run
+            run_backup = backupmgr.run
         else:
-            run_backup = backup_mgr.dry_run
+            run_backup = backupmgr.dry_run
 
-        for name in namespace.backupsets:
+        if not namespace.backupset:
+            self.stderr("Nothing to backup")
+            return 1
+
+        for path in namespace.backupset:
+            if not os.path.isabs(path):
+                path = os.path.join(base_path, path)
+            if not path.endswith('.conf'):
+                path += '.conf'
             try:
-                path = os.path.join(base_path, name)
-                if not path.endswith('.conf'):
-                    path += '.conf'
-                config = load_config(path)
-                job = BackupJob(name, config, spool.next())
+                config = load_backup_config(path)
+            except IOError, exc:
+                self.stderr("Failed to load config %s: %s",
+                            path, exc)
+                return 1
+            try:
+                job = BackupJob(name, config, spool)
                 run_backup(job)
+                print "OKAY?"
             except BackupError, exc:
                 self.stderr("Failed backup '%s': %s", name, exc)
+                print "ARGH"
                 return 1
         else:
             return 0
