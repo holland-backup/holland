bd3842bc54b5951dadbd1d8727b86731ac7e6ecc
Andrew Garner <muzazzi@gmail.com>
2011-01-09 03:23:46 +0000

    add plugin property to BackupJob
    BackupJob.plugin now provides a shortcut to looking up the
    plugin in the job config.  This is identical to:
    job.config['holland:backup']['plugin'] but abstracts away
    this implementation from the rest of the backup api so we can
    more easily change this in the future as needed.

--- holland/core/backup/base.py
+++ holland/core/backup/base.py
@@ -1,29 +1,21 @@
 """Standard Holland Backup API classes"""
 
 from holland.core.plugin import load_plugin, ConfigurablePlugin
-from holland.core.spool import SpoolError
+from holland.core.backup.spool import SpoolError
 from holland.core.backup.error import BackupError
 
 class BackupJob(object):
     """A backup job that may be created and passed to a backup manager in order
     to perform a backup"""
 
-    def __init__(self, name, config, spool):
+    def __init__(self, name, config):
         self.name = name
         self.config = config
-        self.spool = spool
 
-    def load_plugin(self):
+    @property
+    def plugin(self):
         """Load a plugin from this backup job's config"""
-        name = self.config['holland:backup']['plugin']
-        return load_plugin('holland:backup', name)
-
-    def make_path(self):
-        "Create a path for this backup job"
-        try:
-            return self.spool.add(self.name)
-        except SpoolError, exc:
-            raise BackupError("Failed to create backup path: %s" % exc)
+        return self.config['holland:backup']['plugin']
 
 class BackupPlugin(ConfigurablePlugin):
     """Interface that Holland Backup Plugins should conform to"""
