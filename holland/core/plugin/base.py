04fad47d99abf54a0d2ed5cc18294f20b6b5a8fb
Andrew Garner <muzazzi@gmail.com>
2011-01-15 14:41:59 +0000

    cleanup PluginInfo

--- holland/core/plugin/base.py
+++ holland/core/plugin/base.py
@@ -1,41 +1,25 @@
 """Base plugin classes"""
 
-class PluginInfo(object):
+class PluginInfo(dict):
     """Information about a plugin
 
     An instance of this class should be returned
     by a Plugin object's plugin_info() method
     """
-    #: Canonical name of the plugin
-    name = ''
-    #: One-line summary of this plugin's functionality
-    summary = ''
-    #: Textual description of this plugin
-    description = ''
-    #: Name(s) of the author of this plugin
-    author = ''
 
-    # XXX: Versions should be standardized - perhaps per distutils2.version
-    # semantics
-    #: Version for this plugin
-    version = (0, 0, 0)
-    #: Version of holland this plugin was intended for
-    holland_version = (0, 0, 0)
+    def is_compatible(self, required_versoin):
+        from version import NormalizedVersion as V
+        try:
+            return (V(self.api_version) == V(required_version))
+        except AttributeError:
+            return False
 
-    def __init__(self,
-                 name,
-                 summary,
-                 description,
-                 author,
-                 version,
-                 holland_version):
-        self.name = name
-        self.summary = summary
-        self.description = description
-        self.author = author
-        self.version = version
-        self.holland_version = holland_version
-
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError('%s object has no attribute %r' %
+                                 (self.__class__.__name__, name)
 
 class BasePlugin(object):
     """Base class from which all Holland plugins should derive"""
