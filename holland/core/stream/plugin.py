b8ef3b8a26ee059f9d094e98359f39fbddc6cb73
Andrew Garner <muzazzi@gmail.com>
2011-01-15 18:53:57 +0000

    added available_methods() to holland.core.stream
    This method provides an iterable over the names of
    all known stream plugins.  The names are in an arbitrary
    order as provided by the plugin manager's iterate method.
    Each name is suitable for passing to open_stream(...) as
    the method parameter.

--- holland/core/stream/plugin.py
+++ holland/core/stream/plugin.py
@@ -2,6 +2,10 @@
 
 import os
 try:
+    set = set
+except NameError:
+    from sets import Set as set
+try:
     SEEK_SET = os.SEEK_SET
     SEEK_CUR = os.SEEK_CUR
     SEEK_END = os.SEEK_END
@@ -9,12 +13,24 @@ except AttributeError:
     SEEK_SET = 0
     SEEK_CUR = 1
     SEEK_END = 2
-from holland.core.plugin import BasePlugin, PluginError, load_plugin
+from holland.core.plugin import BasePlugin, PluginError, PluginInfo, \
+                                load_plugin, iterate_plugins
 
 
 def load_stream_plugin(name):
     """Load a stream plugin by name"""
     return load_plugin('holland.stream', name)
+
+def available_methods():
+    """List available backup methods as strings
+
+    These names are suitable for passing to open_stream(..., method=name, ...)
+    """
+    results = []
+    for plugin in set(iterate_plugins('holland.stream')):
+        results.append(plugin.name)
+        results.extend(plugin.aliases)
+    return results
 
 def open_stream(filename, mode='r', method=None, *args, **kwargs):
     """Open a stream with the provided method
@@ -32,6 +48,9 @@ def open_stream(filename, mode='r', method=None, *args, **kwargs):
 
 class StreamPlugin(BasePlugin):
     """Base Plugin class"""
+    name = ''
+    aliases = ()
+
     def __init__(self, name):
         self.name = name
 
@@ -53,3 +72,6 @@ class StreamPlugin(BasePlugin):
 class StreamInfo(dict):
     def __str__(self):
         return self.description
+
+class StreamError(IOError):
+    """Exception in stream"""
