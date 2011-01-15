a87e1bc4e79f64f43777b9791ed001d9d1a60006
Andrew Garner <muzazzi@gmail.com>
2011-01-15 18:56:17 +0000

    clip the duplicate plugin code from holland.core.stream
    Refactored the plugin code into a separate module in
    holland.core.stream.plugin.  This is exported by the
    holland.core.stream package and some redundant definitions
    in other submodules of that package have been removed.

--- holland/core/stream/__init__.py
+++ holland/core/stream/__init__.py
@@ -1,23 +1,4 @@
 
 """Generic support for loading file-like objects"""
 
-from holland.core.plugin import load_plugin, PluginError
-
-
-def load_stream_plugin(name):
-    """Load a stream plugin by name"""
-    return load_plugin('holland.stream', name)
-
-def open_stream(filename, mode='r', method=None, *args, **kwargs):
-    """Open a stream with the provided method
-
-    If not method is provided, this will default to the builtin file
-    object
-    """
-    if method is None:
-        method = 'file'
-    try:
-        stream = load_stream_plugin(method)(method)
-    except PluginError, exc:
-        raise IOError("No stream found for method %r: %s" % (method, exc))
-    return stream.open(filename, mode, *args, **kwargs)
+from holland.core.stream.plugin import *
