7cb0f104accf8d5a10493540c703c4cde6fd31b6
Andrew Garner <muzazzi@gmail.com>
2011-01-16 18:33:30 +0000

    make chained_exc in BackupError optional

--- holland/core/backup/error.py
+++ holland/core/backup/error.py
@@ -3,9 +3,12 @@
 class BackupError(Exception):
     """Base exception all backup errors derive from"""
 
-    def __init__(self, message, chained_exc):
+    def __init__(self, message, chained_exc=None):
         self.message = message
         self.chained_exc = chained_exc
 
     def __str__(self):
-        return "%s: %s" % (self.message, self.chained_exc)
+        msg = "%s" % self.message
+        if self.chained_exc:
+            msg += ": %s" % self.chained_exc
+        return msg
