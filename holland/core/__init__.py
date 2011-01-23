a9873c2ded67740a53818b4249a6e295b0888378
Andrew Garner <muzazzi@gmail.com>
2011-01-23 07:45:43 +0000

    update holland.core package exports

--- holland/core/__init__.py
+++ holland/core/__init__.py
@@ -1,6 +1,9 @@
 """Holland core API"""
 
 from holland.core.plugin import load_plugin, iterate_plugins, BasePlugin, PluginError
+from holland.core.config import Config, Configspec
+from holland.core.hooks import BaseHook
+from holland.core.stream import open_stream
 from holland.core.backup import *
 
 __all__ = [
@@ -8,6 +11,10 @@ __all__ = [
     'load_plugin',
     'iterate_plugins',
     'PluginError',
+    'Config',
+    'Configspec',
+    'BaseHook',
+    'open_stream',
     'BackupSpool',
     'BackupStore',
     'BackupManager',
