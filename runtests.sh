f5c237969e82c1a2e519a72c4dd09736f4e38be5
Andrew Garner <muzazzi@gmail.com>
2011-01-23 11:41:30 +0000

    make runtests a little more portable

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+set -u
+set -x
+set -e
+
 export PYLINTRC=$PWD/.pylintrc
 nosetests -v --with-xunit
-coverage xml
+coverage=$(which coverage 2>/dev/null)
+if [ $? -ne 0 ]
+then
+coverage=python-coverage
+fi
+$coverage xml
 pylint -f parseable holland > pylint.txt
