bf3bbcd07c3b2ac59953d2e2f4ec50462cf6a1d2
Andrew Garner <muzazzi@gmail.com>
2011-01-23 07:19:30 +0000

    remove unnecessary backports

--- holland/cli/backports/__init__.py
+++ holland/cli/backports/__init__.py
@@ -3,22 +3,12 @@ import sys
 if sys.version_info < (3, 0):
     import argparse
     import subprocess
-    import logging
-    from logging import handlers as logging_handlers
-    from logging import config as logging_config
-    import configobj
-    import validate
 
 def setup_backports():
     if sys.version_info < (3, 0):
         module_list = [
             'argparse',
             'subprocess',
-            'logging',
-            'configobj',
-            'validate',
         ]
         for module in module_list:
             sys.modules[module] = globals()[module]
-        sys.modules['logging.handlers'] = logging_handlers
-        sys.modules['logging.config'] = logging_config
