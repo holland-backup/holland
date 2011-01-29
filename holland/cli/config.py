1b193b710fdf47fc7c3615c1ca791a4adddbf32d
Andrew Garner <muzazzi@gmail.com>
2011-01-29 19:19:17 +0000

    add missing imports to holland.cli.config

--- holland/cli/config.py
+++ holland/cli/config.py
@@ -1,5 +1,7 @@
+"""Load a holland.conf config file"""
+import os
 import logging
-from holland.core import Config, Configspec
+from holland.core import Config, Configspec, ConfigError
 from holland.core import load_plugin
 
 LOG = logging.getLogger(__name__)
@@ -19,9 +21,14 @@ level            = log_level(default="info")
 """.splitlines())
 
 class GlobalHollandConfig(Config):
+    name = None
+
+    def basedir(self):
+        return os.path.abspath(os.path.dirname(self.name or '.'))
+
     def load_backupset(self, name):
         if not os.path.isabs(name):
-            name = os.path.join(self.name, name)
+            name = os.path.join(self.basedir(), name)
         if not name.endswith('.conf'):
             name += '.conf'
 
@@ -30,11 +37,12 @@ class GlobalHollandConfig(Config):
         # load providers/$plugin.conf if available
         plugin = cfg.get('holland:backup', {}).get('plugin')
         if plugin:
-            provider_path = os.path.join(self.name, 'providers', plugin)
+            provider_path = os.path.join(self.basedir(), 'providers', plugin)
             try:
                 cfg.meld(Config.read([provider_path]))
             except ConfigError:
                 LOG.debug("No global provider found.  Skipping.")
+        cfg.name = os.path.splitext(os.path.basename(name))[0]
         return cfg
 
     #@classmethod
@@ -45,10 +53,9 @@ class GlobalHollandConfig(Config):
 def load_global_config(path):
     if path:
         cfg = GlobalHollandConfig.read([path])
-        cfg.path = path
+        cfg.name = path
     else:
         cfg = GlobalHollandConfig()
-        cfg.path = os.getcwd()
 
     cfg.configspec().validate(cfg)
     return cfg
