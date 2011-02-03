cd3ec3b065ff5d342f3b5df5db884a0e78debf0e
Andrew Garner <muzazzi@gmail.com>
2011-02-03 05:18:26 +0000

    fix BackupManager.cleanup

--- holland/core/backup/manager.py
+++ holland/core/backup/manager.py
@@ -10,6 +10,7 @@ from holland.core.backup.util import load_backup_plugin
 from holland.core.backup.spool import BackupSpool
 from holland.core.backup.job import BackupJob
 from holland.core.backup.base import BackupPlugin
+from holland.core.config import Config
 
 LOG = logging.getLogger(__name__)
 
@@ -37,10 +38,9 @@ class BackupManager(object):
 
     def cleanup(self, path):
         """Run a plugin's cleanup method"""
-        from holland.core.config import load_config
-        store = self.spool.load(path)
-        config = load_config(os.path.join(store.path, 'backup.conf'))
-        plugincls = load_backup_plugin(config)
-        plugin = plugincls(store)
+        store = self.spool.load_store(path)
+        config = Config.read([os.path.join(store.path, 'backup.conf')])
+        plugin = load_backup_plugin(config)
+        plugin.setup(store)
         plugin.configure(config)
         plugin.cleanup()
