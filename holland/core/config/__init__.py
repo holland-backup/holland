1a3384608d510b85d4cad24ce1f59f0883b4a2be
Andrew Garner <muzazzi@gmail.com>
2011-02-10 12:05:05 +0000

    export ValidationError exception in holland.core.config

--- holland/core/config/__init__.py
+++ holland/core/config/__init__.py
@@ -1,5 +1,5 @@
 """Holland Config support"""
 
 from holland.core.config.config import Config, ConfigError
-from holland.core.config.spec import Configspec
+from holland.core.config.spec import Configspec, ValidationError
 from holland.core.config.parsing import CheckParser
