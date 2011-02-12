f60e768c738147a329d520b56db4476fe745ca26
Andrew Garner <muzazzi@gmail.com>
2011-02-12 23:02:08 +0000

    use ValidateError for ConfigurablePlugin rather than ValidationError

--- holland/core/plugin/base.py
+++ holland/core/plugin/base.py
@@ -1,6 +1,6 @@
 """Base plugin classes"""
 import logging
-from holland.core.config import Configspec, ValidationError
+from holland.core.config import Configspec, ValidateError
 
 LOG = logging.getLogger(__name__)
 
@@ -36,7 +36,7 @@ class ConfigurablePlugin(BasePlugin):
         """Configure this plugin with the given dict-like object"""
         try:
             self.config = self.configspec().validate(config)
-        except ValidationError, exc:
+        except ValidateError, exc:
             LOG.error("%d errors found while validating configuration",
                     len(exc.errors))
             for error in exc.errors:
