76be47e82e4b7c671be9e1319acf2e730c162a57
Andrew Garner <muzazzi@gmail.com>
2011-02-13 01:12:41 +0000

    set the name for the root Config to 'root'

--- holland/core/config/config.py
+++ holland/core/config/config.py
@@ -35,6 +35,9 @@ class Config(OrderedDict):
     cont_cre        = re.compile(r'\s+(?P<value>.+?)$')
     include_cre     = re.compile(r'%include (?P<name>.+?)\s*$')
 
+    #: name of the section.  The root config will always have the name 'root'
+    name = 'root'
+
     #: path where this configuration was loaded from or None if not loaded from
     #  a file
     path = None
