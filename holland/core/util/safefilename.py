444162e4b0bb6eb567143232ac62c41193170c3d
Andrew Garner <muzazzi@gmail.com>
2011-02-14 02:06:51 +0000

    cleanup safefilename
    - remove codec functionality as this transformation is not strictly
      a codec

--- holland/core/util/safefilename.py
+++ holland/core/util/safefilename.py
@@ -3,7 +3,6 @@
 Based on the identically named codec from the bobcat project
 """
 
-import codecs
 try:
     bytes
 except NameError:
@@ -11,8 +10,8 @@ except NameError:
 
 from holland.core.util.pycompat import Scanner
 
-def decode(input, errors='strict'):
-    """Decode a filename into the original unicode string"""
+def decode(filename):
+    """Decode a filename to the original filename"""
     scanner = Scanner([
         (b"[a-z0-9-+!$%&\'@~#.,^]+", lambda s, t: bytes(t).decode('ascii')),
         (b"_", lambda s, t: ' '),
@@ -20,13 +19,14 @@ def decode(input, errors='strict'):
         (b'\([0-9a-f]{1,8}\)', lambda s, t: chr(int(bytes(t[1:-1]), 16))),
     ])
 
-    data, remaining = scanner.scan(input)
+    data, remaining = scanner.scan(data)
 
-    # XXX: error if len(remaining) > 0
+    if remaining:
+        raise ValueError("Failed to decode %s" % data)
 
     return ''.join(data), len(remaining)
 
-def encode(input, errors='strict'):
+def encode(filename):
     """Encode the unicode string into a filename"""
     scanner = Scanner([
         (r"[a-z0-9-+!$%&\'@~#.,^]+", lambda s, t: t.encode('ascii')),
@@ -36,52 +36,9 @@ def encode(input, errors='strict'):
          lambda s, t: ('(%s)' % hex(ord(t))[2:]).encode('ascii')),
     ])
 
-    data, remaining = scanner.scan(input)
+    data, remaining = scanner.scan(data)
 
-    # XXX: error if len(remaining) > 0
+    if remaining:
+        raise ValueError("Failed to decode %s" % data)
 
     return ''.encode('ascii').join(data), len(remaining)
-
-class Codec(codecs.Codec):
-    """Codec class for safefilename"""
-    encode = staticmethod(encode)
-
-    decode = staticmethod(decode)
-
-class IncrementalEncoder(codecs.IncrementalEncoder):
-    """IncrementEncoder"""
-    def encode(self, input, final=False):
-        return encode(input, self.errors)[0]
-
-class IncrementalDecoder(codecs.IncrementalDecoder):
-    """IncrementalDecoder"""
-    def decode(self, input, final=False):
-        return encode(input, self.errors)[0]
-
-class StreamWriter(Codec, codecs.StreamWriter):
-    """StreamWriter"""
-    pass
-
-class StreamReader(Codec, codecs.StreamReader):
-    """StreamReader"""
-    pass
-
-### encodings module API
-
-def getregentry():
-    """codec info used to register the safefilename codec with python"""
-    return codecs.CodecInfo(
-        name='safefilename',
-        encode=Codec.encode,
-        decode=Codec.decode,
-        incrementalencoder=IncrementalEncoder,
-        incrementaldecoder=IncrementalDecoder,
-        streamwriter=StreamWriter,
-        streamreader=StreamReader,
-    )
-
-def search_function(encoding):
-    """safefilename codec registration function"""
-    if encoding == 'safefilename':
-        return getregentry()
-codecs.register(search_function)
