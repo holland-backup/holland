1dfcaec3febea4db7115cfda0683f525026de47d
Andrew Garner <muzazzi@gmail.com>
2011-02-14 01:22:59 +0000

    add holland to pkg_resources.working_set in tests/
    - to ensure we can run tests and find builtin holland plugins
      add the main holland egg to the working set in the root tests
      package

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
 #
+import os
+from pkg_resources import *
+
+def setup():
+    """Pull in the main holland egg into pkg_resources"""
+    path = os.path.dirname(os.path.dirname(__file__))
+    env = Environment([path])
+    dists, errors = working_set.find_plugins(env)
+    for dist in dists:
+        working_set.add(dist)
