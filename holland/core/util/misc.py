ec164014a5051db4658afe1321c40806817d0005
Andrew Garner <muzazzi@gmail.com>
2011-02-27 22:48:49 +0000

    cleanup run_command
    - avoid trapping OSError.  this should be caught by the caller and
      is unlikely to be raised anyway as /bin/sh will generate an error
      rather than execve

--- holland/core/util/misc.py
+++ holland/core/util/misc.py
@@ -7,15 +7,9 @@ def run_command(cmd):
     :param cmd: string command
     :returns: 2-tuple (stdout_string, stderr_string)
     """
-    try:
-        process = Popen(cmd,
-                        shell=True,
-                        stdout=PIPE,
-                        stderr=PIPE,
-                        close_fds=True)
-    except OSError, exc:
-        raise
-
-    stdout, stderr = process.communicate()
-
-    return stdout, stderr
+    process = Popen(cmd,
+                    shell=True,
+                    stdout=PIPE,
+                    stderr=PIPE,
+                    close_fds=True)
+    return process.communicate()
