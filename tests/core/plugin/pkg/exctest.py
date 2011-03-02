815a607f69dac37a398392de9751e152e04295e7
Andrew Garner <muzazzi@gmail.com>
2011-03-02 10:20:03 -0500

    adjust test package for plugin manager tests
    - nose on py2.3/rhel4 seems to interfere with trying to force an
      error on import.  exceptions are now raised in the test plugin
      constructors to still test the same error handling path in
      the plugin manager

--- tests/core/plugin/pkg/exctest.py
+++ tests/core/plugin/pkg/exctest.py
@@ -1,1 +1,6 @@
-raise SystemExit('test')
+import sys
+
+class Foo(object):
+    def __init__(self, name):
+        # test error handling by plugin manager
+        sys.exit(1)
--- tests/core/plugin/pkg/fail.py
+++ tests/core/plugin/pkg/fail.py
@@ -1,3 +1,3 @@
-raise ValueError("This module will always raise an error on import")
-
-class NoopPlugin(object): pass
+class NoopPlugin(object):
+    def __init__(self, name):
+        raise ValueError("This plugin will always raise an error on load")
