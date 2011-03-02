3d46b1c22dcec6dd75c452ff642843018027ed74
Andrew Garner <muzazzi@gmail.com>
2011-03-02 10:40:46 -0500

    split jenkins/runtests scripts
    - runtests now only runs nose (optionally with coverage info)
    - jenkins.sh now generates xunit/pylint/coverage info for various
      jenkins plugins

---
+++ jenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -u
+set -x
+
+python setup.py egg_info
+
+export PYLINTRC=$PWD/.pylintrc
+nosetests -v --with-xunit tests
+which coverage > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+coverage=python-coverage
+else
+coverage=coverage
+fi
+$coverage xml
+export PYTHONPATH="$PWD/holland/cli/backports/"
+
+pylint -f parseable holland > pylint.txt
+
+exit 0
--- runtests.sh
+++ runtests.sh
@@ -3,19 +3,4 @@ set -u
 set -x
 
 python setup.py egg_info
-
-export PYLINTRC=$PWD/.pylintrc
 nosetests -v tests
-which coverage > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-coverage=python-coverage
-else
-coverage=coverage
-fi
-$coverage xml
-export PYTHONPATH="$PWD/holland/cli/backports/"
-
-pylint -f parseable holland > pylint.txt
-
-exit 0
