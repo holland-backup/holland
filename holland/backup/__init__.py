b826548ed79d513bb07d5d106aa4cd4603240e62
Andrew Garner <muzazzi@gmail.com>
2011-04-04 14:35:42 +0000

    improve module docstrings for holland.backup and holland.lib root packages

--- holland/backup/__init__.py
+++ holland/backup/__init__.py
@@ -1,1 +1,10 @@
-"""Holland Backup Plugin package"""
+"""
+    holland.backup
+    ~~~~~~~~~~~~~~
+
+    This package is a namespace for holland backup plugins that are distributed
+    separately from the main holland distribution.
+
+    :copyright: 2008-2011 Rackspace US, Inc.
+    :license: BSD, see LICENSE.rst for details
+"""
--- holland/lib/__init__.py
+++ holland/lib/__init__.py
@@ -1,1 +1,10 @@
-"""Package where for external holland libraries"""
+"""
+    holland.lib
+    ~~~~~~~~~~~
+
+    This package is a namespace for holland libraries that are distributed
+    separately from the main holland distribution.
+
+    :copyright: 2008-2011 Rackspace US, Inc.
+    :license: BSD, see LICENSE.rst for details
+"""
