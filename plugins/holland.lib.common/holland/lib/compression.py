39acd7e8b61bff395a54438c03feba778b8522b1
Andrew Garner <muzazzi@gmail.com>
2011-05-16 04:04:28 +0000

    add py2.4 fallbacks for check_call in holland.lib.compression

--- plugins/holland.lib.common/holland/lib/compression.py
+++ plugins/holland.lib.common/holland/lib/compression.py
@@ -3,7 +3,43 @@
 import os
 import errno
 from tempfile import TemporaryFile, mkstemp
-from subprocess import Popen, PIPE, STDOUT, check_call, CalledProcessError
+from subprocess import Popen, PIPE, STDOUT
+try:
+    from subprocess import check_call, CalledProcessError
+except ImportError:
+    from subprocess import call
+
+    class CalledProcessError(Exception):
+        """This exception is raised when a process run by check_call() or
+        check_output() returns a non-zero exit status.
+        The exit status will be stored in the returncode attribute;
+        check_output() will also store the output in the output attribute.
+        """
+        def __init__(self, returncode, cmd, output=None):
+            self.returncode = returncode
+            self.cmd = cmd
+            self.output = output
+        def __str__(self):
+            return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
+
+    def check_call(*popenargs, **kwargs):
+        """Run command with arguments.  Wait for command to complete.  If
+        the exit code was zero then return, otherwise raise
+        CalledProcessError.  The CalledProcessError object will have the
+        return code in the returncode attribute.
+
+        The arguments are the same as for the Popen constructor.  Example:
+
+        check_call(["ls", "-l"])
+        """
+        retcode = call(*popenargs, **kwargs)
+        if retcode:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = popenargs[0]
+            raise CalledProcessError(retcode, cmd)
+        return 0
+
 from holland.core.stream import StreamPlugin, StreamError
 from holland.core.stream.base import RealFileLike
 
