330def7b766bf7253ee0d722a25c769090a14581
Andrew Garner <muzazzi@gmail.com>
2011-05-17 15:11:58 +0000

    adding script plugin to holland-1.1

---
+++ plugins/holland.backup.script/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0.7a1'
+
+setup(name='holland-script',
+      version=version,
+      description="Perform backups with simple shell commands",
+      long_description="""
+      """,
+      author='Andrew Garner',
+      author_email='muzazzi@gmail.com',
+      url='http://hollandbackup.org',
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      zip_safe=False,
+      entry_points="""
+      [holland.backup]
+      script = holland.backup.script:ScriptPlugin
+      """,
+      namespace_packages=['holland', 'holland.backup']
+)
