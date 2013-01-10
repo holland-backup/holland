20dff97308cd72f11d2df4b42c98a05e6e34e5ac
Andrew Garner <muzazzi@gmail.com>
2013-01-10 12:59:21 -0500

    suppress snapshot temporary mountpoint in dry-run mode

--- plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/plugin/raw/plugin.py
+++ plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/plugin/raw/plugin.py
@@ -106,7 +106,7 @@ class MysqlLVMBackup(object):
     def configspec(self):
         """INI Spec for the configuration values this plugin supports"""
         return self.CONFIGSPEC
-    
+
     def backup(self):
         """Run a backup by running through a LVM snapshot against the device
         the MySQL datadir resides on
@@ -130,7 +130,7 @@ class MysqlLVMBackup(object):
 
         # create a snapshot manager
         snapshot = build_snapshot(self.config['mysql-lvm'], volume,
-                                  suppress_tmpdir=True)
+                                  suppress_tmpdir=self.dry_run)
         # calculate where the datadirectory on the snapshot will be located
         rpath = relpath(datadir, getmount(datadir))
         snap_datadir = os.path.abspath(os.path.join(snapshot.mountpoint, rpath))
