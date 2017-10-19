2703d732f1978663e8deeb94ed60483b9b503d4a
Jose Arthur Benetasso Villanova <jose.arthur@locaweb.com.br>
2017-10-19 16:26:32 -0200

    Consider old backups an error too

--- holland/commands/nagios.py
+++ holland/commands/nagios.py
@@ -1,20 +1,27 @@
 from collections import namedtuple
+from datetime import timedelta, datetime
 
 from holland.core.spool import spool
 from holland.core.config import hollandcfg
 
-from holland.core.command import Command
+from holland.core.command import Command, option
+
 
 class Nagios(Command):
-    """${cmd_usage}                                 
+    """${cmd_usage}
 
-    ${cmd_option_list}                              
+    ${cmd_option_list}
 
-    Check backup retention                          
-    """                                             
-    name = 'nagios'                                 
-    aliases = []                                    
-    description = 'Check backup retention'          
+    Check backup retention
+    """
+    name = 'nagios'
+    aliases = []
+    description = 'Check backup retention'
+    options = [
+            option('-t', '--threshold', type="int", default=2,
+                help="If all backups in a backupset are older than this " +
+                "number in days consider out of retention")
+    ]
 
     def run(self, cmd, opts, *backupsets):
         if not backupsets:
@@ -26,7 +33,8 @@ class Nagios(Command):
             print "HOLLAND WARNING - No backupsets"
             return 0
 
-        Retention = namedtuple("Retention", ["backupset", "result", "message"])
+        Retention = namedtuple("Retention",
+                ["backupset", "result", "message"])
         info = set()
         for name in backupsets:
             try:
@@ -37,10 +45,26 @@ class Nagios(Command):
             # ensure we have at least an empty holland:backup section
             config.setdefault('holland:backup', {})
 
-            backups = len(list(spool.list_backups(name)))
-            retention = int(config['holland:backup']['backups-to-keep'])
-            message = "{} of {}".format(backups, retention)
-            info.add(Retention(name, backups >= retention, message))
+            backups = list(spool.list_backups(name))
+
+            most_recent = datetime.fromtimestamp(float(0))
+            for backup in backups:
+                backup.load_config()
+                str_d = backup.config['holland:backup']['stop-time']
+                d = datetime.fromtimestamp(float(str_d))
+                if d > most_recent:
+                    most_recent = d
+
+            n_retention = int(config['holland:backup']['backups-to-keep'])
+            message = "{} of {}".format(len(backups), n_retention)
+            status = len(backups) >= n_retention
+
+            threshold = datetime.now() - timedelta(days=opts.threshold)
+            if most_recent < threshold:
+                message += '; backups too old: expected {} got {}'.format(
+                        threshold, most_recent)
+                status = False
+            info.add(Retention(name, status, message))
 
         errors = [x for x in info if not x.result]
 
