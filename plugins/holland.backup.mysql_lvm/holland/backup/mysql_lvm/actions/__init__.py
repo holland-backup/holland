e8ef7f79434f7894beb652963ffe9a79301e13e6
Chris Derichs <chris.derichs@rackspace.com>
2017-11-15 23:30:38 -0600

    Added option to copy datadir instead of tar for LVM snapshots.

--- plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/actions/__init__.py
+++ plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/actions/__init__.py
@@ -1,2 +1,3 @@
 from holland.backup.mysql_lvm.actions.mysql import *
 from holland.backup.mysql_lvm.actions.tar import *
+from holland.backup.mysql_lvm.actions.dir import *
---
+++ plugins/holland.backup.mysql_lvm/holland/backup/mysql_lvm/actions/dir.py
@@ -0,0 +1,49 @@
+import os
+import time
+import shlex
+import signal
+import logging
+from subprocess import list2cmdline, Popen, CalledProcessError
+from holland.core.exceptions import BackupError
+
+LOG = logging.getLogger(__name__)
+
+class DirArchiveAction(object):
+    def __init__(self, snap_datadir, backup_datadir, config):
+        self.snap_datadir = snap_datadir
+        self.backup_datadir = backup_datadir
+        self.config = config
+
+    def __call__(self, event, snapshot_fsm, snapshot_vol):
+        argv = [
+            'cp',
+            '--archive',
+            self.snap_datadir,
+            '-t',
+            self.backup_datadir
+        ]
+
+        LOG.info("Running: %s ", list2cmdline(argv))
+        archive_log = os.path.join(self.backup_datadir, '../archive.log')
+
+        process = Popen(argv,
+                        preexec_fn=os.setsid,
+                        stdout=open(archive_log, 'w'),
+                        stderr=open(archive_log, 'w'),
+                        close_fds=True)
+        while process.poll() is None:
+            if signal.SIGINT in snapshot_fsm.sigmgr.pending:
+                os.kill(process.pid, signal.SIGKILL)
+            time.sleep(0.5)
+
+        if signal.SIGINT in snapshot_fsm.sigmgr.pending:
+            raise KeyboardInterrupt("Interrupted")
+
+        if process.returncode != 0:
+            LOG.error("tar exited with non-zero status: %d",
+                      process.returncode)
+            LOG.error("Tailing up to the last 10 lines of archive.log for "
+                      "troubleshooting:")
+            for line in open(archive_log, 'r').readlines()[-10:]:
+                LOG.error(" ! %s", line.rstrip())
+            raise CalledProcessError(process.returncode, "tar")
