f4df8541331181121f219eb1ee0a3c2178211198
Steven Soulen <steven.soulen@rackspace.com>
2018-08-08 13:33:32 -0500

    Inadvertently reveted this: Remove mysqlconnect as this seems to conflict with MySQL-python

--- plugins/holland.lib.mysql/setup.py
+++ plugins/holland.lib.mysql/setup.py
@@ -6,7 +6,6 @@ setup(name='holland.lib.mysql',
       version=version,
       description="Holland MySQL Support",
       install_requires=[
-          'mysqlclient'
       ],
       long_description="""
         Provides convenience methods for MySQL
