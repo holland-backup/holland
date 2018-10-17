6fc220bfd96876bb0f86d9352dab59f9b38e376a
soulen3 <steven@soulen.net>
2018-10-17 17:42:36 -0500

    Travis xtrabackups (#218)
    * Add xtrabackup to travis
    
    * fix copy and paste error
    
    * try newer version of xtrabackups
    
    * Add percona repo

--- scripts/travis_ci.sh
+++ scripts/travis_ci.sh
@@ -32,11 +32,15 @@ CMDS=(
 "holland mc --name mysqldump mysqldump"
 "holland bk mysqldump --dry-run"
 "holland bk mysqldump"
+"holland mc --name xtrabackup xtrabackup"
+"holland bk xtrabackup --dry-run"
+"holland bk xtrabackup"
 "holland_cvmysqlsv -bkplevel 1 -attempt 1 -job 123456 -cn 957072-661129 -vm Instance001 --bkset mysqldump"
 )
 
 for command in "${CMDS[@]}"
 do
+    sleep 1
     $command
     exit_code=$?
 	if [ $exit_code -ne  0 ]
