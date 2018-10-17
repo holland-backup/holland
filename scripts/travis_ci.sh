b40d8e57cce174ae237b05a5e29f58f3e4d5a99d
Steven Soulen <steven.soulen@rackspace.com>
2018-10-17 16:23:57 -0500

    use virtualenv

--- scripts/travis_ci.sh
+++ scripts/travis_ci.sh
@@ -4,7 +4,7 @@
 for i in `ls -d plugins/holland.*`
 do
     cd $TRAVIS_BUILD_DIR/${i}
-    python setup.py install --prefix=/usr/bin/ 2>>/dev/null >>/dev/null
+    python setup.py install 2>>/dev/null >>/dev/null
     exit_code=$?
 	if [ $exit_code -ne  0 ]
     then
@@ -13,7 +13,7 @@ do
     fi
 done
 cd $TRAVIS_BUILD_DIR/contrib/holland-commvault/
-python setup.py install --prefix=/usr/bin/ 2>>/dev/null >>/dev/null
+python setup.py install 2>>/dev/null >>/dev/null
 exit_code=$?
 if [ $exit_code -ne  0 ]
 then
@@ -27,10 +27,10 @@ cp $TRAVIS_BUILD_DIR/config/providers/* /etc/holland/providers/
 
 
 CMDS=(
-"/usr/bin/holland mc --name mysqldump mysqldump"
-"/usr/bin/holland bk mysqldump --dry-run"
-"/usr/bin/holland bk mysqldump"
-"/usr/bin/holland_cvmysqlsv -bkplevel 1 -attempt 1 -job 123456 -cn 957072-661129 -vm Instance001 --bkset mysqldump"
+"~travis/bin/holland mc --name mysqldump mysqldump"
+"~travis/bin/holland bk mysqldump --dry-run"
+"~travis/bin/holland bk mysqldump"
+"~travis/bin/holland_cvmysqlsv -bkplevel 1 -attempt 1 -job 123456 -cn 957072-661129 -vm Instance001 --bkset mysqldump"
 )
 
 for command in "${CMDS[@]}"
