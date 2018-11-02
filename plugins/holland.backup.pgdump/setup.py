ba51008601b9824089f0bd8b4f6f985815a33749
Steven Soulen <steven.soulen@rackspace.com>
2018-11-02 12:57:59 -0500

    Fix copy and paste error

--- plugins/holland.backup.pgdump/setup.py
+++ plugins/holland.backup.pgdump/setup.py
@@ -16,8 +16,7 @@ setup(name='holland.backup.pgdump',
                                       'tests.*']),
       namespace_packages=['holland', 'holland.backup'],
       zip_safe=True,
-      psycopg2
-      install_requires=[ "psycopg2" ],
+      install_requires=['psycopg2' ],
       # holland looks for plugins in holland.backup
       entry_points="""
       [holland.backup]
