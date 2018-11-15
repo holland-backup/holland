05686f60f7c3fb2d0c1723463bbc3aa35566a481
Steven Soulen <steven.soulen@rackspace.com>
2018-11-15 17:17:24 -0600

    add mk_config tests, and fix arg parse errors in mk_config commands

--- scripts/travis_ci.sh
+++ scripts/travis_ci.sh
@@ -32,17 +32,22 @@ su -c 'psql -c "CREATE USER root WITH SUPERUSER"' postgres
 
 CMDS=(
 "holland mc --name mysqldump mysqldump"
+"holland mc -f /tmp/mysqldump.conf mysqldump"
 "holland bk mysqldump --dry-run"
 "holland bk mysqldump"
 "holland mc --name xtrabackup xtrabackup"
+"holland mc --file /tmp/xtrabackup.conf xtrabackup"
 "holland bk xtrabackup --dry-run"
 "holland bk xtrabackup"
 "holland mc --name mongodump mongodump"
+"holland mc --file /tmp/mongodump.conf mongodump"
 "holland bk mongodump --dry-run"
 "holland bk mongodump"
 "holland mc --name pgdump pgdump"
+"holland mc --file /tmp/pgdump.conf pgdump"
 "holland bk pgdump --dry-run"
 "holland bk pgdump"
+"holland bk mysqldump xtrabackup"
 "holland_cvmysqlsv -bkplevel 1 -attempt 1 -job 123456 -cn 957072-661129 -vm Instance001 --bkset mysqldump"
 )
 
