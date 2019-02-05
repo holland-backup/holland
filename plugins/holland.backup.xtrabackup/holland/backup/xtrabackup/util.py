bce1566a94b4dbe73c9a2e727f80ddd6280c234a
Steven Soulen <steven.soulen@rackspace.com>
2019-02-05 12:53:20 -0600

    Fix formating

--- plugins/holland.backup.xtrabackup/holland/backup/xtrabackup/util.py
+++ plugins/holland.backup.xtrabackup/holland/backup/xtrabackup/util.py
@@ -55,9 +55,7 @@ def generate_defaults_file(defaults_file, include=(), auth_opts=None):
         finally:
             fileobj.close()
     except IOError as exc:
-        raise BackupError(
-            "Failed to create %s: [%d] %s" % (defaults_file, exc.errno, exc.strerror)
-        )
+        raise BackupError("Failed to create %s: [%d] %s" % (defaults_file, exc.errno, exc.strerror))
 
     return defaults_file
 
@@ -82,9 +80,7 @@ def run_xtrabackup(args, stdout, stderr):
 
     if process.returncode != 0:
         # innobackupex exited with non-zero status
-        raise BackupError(
-            "innobackupex exited with failure status [%d]" % process.returncode
-        )
+        raise BackupError("innobackupex exited with failure status [%d]" % process.returncode)
 
 
 def apply_xtrabackup_logfile(xb_cfg, backupdir, binary_xtrabackup=False):
@@ -97,37 +93,35 @@ def apply_xtrabackup_logfile(xb_cfg, backupdir, binary_xtrabackup=False):
         return
 
     if "--compress" in xb_cfg["additional-options"]:
-        LOG.warning(
-            "Skipping --apply-logs since --compress option appears "
-            "to have been used."
-        )
+        LOG.warning("Skipping --apply-logs since --compress option appears " "to have been used.")
         return
 
     if binary_xtrabackup:
         innobackupex = which("xtrabackup")
-    	args = [innobackupex, "--prepare",  "--apply-log-only", "--target-dir=" + backupdir + "/data"]
+        args = [
+            innobackupex,
+            "--prepare",
+            "--apply-log-only",
+            "--target-dir=" + backupdir + "/data",
+        ]
     else:
         innobackupex = xb_cfg["innobackupex"]
         if not isabs(innobackupex):
             innobackupex = which(innobackupex)
-    	args = [innobackupex, "--apply-log", backupdir]
+        args = [innobackupex, "--apply-log", backupdir]
 
     cmdline = list2cmdline(args)
     LOG.info("Executing: %s", cmdline)
     try:
         process = Popen(args, stdout=PIPE, stderr=STDOUT, close_fds=True)
     except OSError as exc:
-        raise BackupError(
-            "Failed to run %s: [%d] %s" % cmdline, exc.errno, exc.strerror
-        )
+        raise BackupError("Failed to run %s: [%d] %s" % cmdline, exc.errno, exc.strerror)
 
     for line in process.stdout:
         LOG.info("%s", line.rstrip())
     process.wait()
     if process.returncode != 0:
-        raise BackupError(
-            "%s returned failure status [%d]" % (cmdline, process.returncode)
-        )
+        raise BackupError("%s returned failure status [%d]" % (cmdline, process.returncode))
 
 
 def determine_stream_method(stream):
@@ -161,9 +155,7 @@ def execute_pre_command(pre_command, **kwargs):
     pre_command = Template(pre_command).safe_substitute(**kwargs)
     LOG.info("Executing pre-command: %s", pre_command)
     try:
-        process = Popen(
-            pre_command, stdout=PIPE, stderr=STDOUT, shell=True, close_fds=True
-        )
+        process = Popen(pre_command, stdout=PIPE, stderr=STDOUT, shell=True, close_fds=True)
     except OSError as exc:
         # missing executable
         raise BackupError("pre-command %s failed: %s" % (pre_command, exc.strerror))
@@ -242,7 +234,7 @@ def build_xb_args(config, basedir, defaults_file=None, binary_xtrabackup=False):
             args.append("--ibbackup=" + ibbackup)
         if stream:
             args.append("--stream=xbstream")
-	else:
+        else:
             args.append("--target-dir=%s/data" % basedir)
         if slave_info:
             args.append("--slave-info")
@@ -267,9 +259,7 @@ def xtrabackup_version():
     try:
         process = Popen(xb_version, stdout=PIPE, stderr=STDOUT, close_fds=True)
     except OSError as exc:
-        raise BackupError(
-            "Failed to run %s: [%d] %s" % cmdline, exc.errno, exc.strerror
-        )
+        raise BackupError("Failed to run %s: [%d] %s" % cmdline, exc.errno, exc.strerror)
 
     for line in process.stdout:
         if isinstance(line, bytes):
@@ -280,7 +270,5 @@ def xtrabackup_version():
 
     process.wait()
     if process.returncode != 0:
-        raise BackupError(
-            "%s returned failure status [%d]" % (cmdline, process.returncode)
-        )
+        raise BackupError("%s returned failure status [%d]" % (cmdline, process.returncode))
     return xtrabackup_version
