8ac0b636754a74db2d1f5aa5c3cf1d945b5a1917
Steven Soulen <steven.soulen@rackspace.com>
2019-04-15 10:32:27 -0500

    only set a version for psycopg2 for python version less than 2.7

--- plugins/holland.backup.pgdump/setup.py
+++ plugins/holland.backup.pgdump/setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = "1.1.14"
+if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+    install_requires = (["psycopg2==2.7.7"],)
+else:
+    install_requires = (["psycopg2"],)
 
 setup(
     name="holland.backup.pgdump",
@@ -16,7 +20,7 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests", "tests.*"]),
     namespace_packages=["holland", "holland.backup"],
     zip_safe=True,
-    install_requires=["psycopg2==2.7.7"],
+    install_requires=install_requires,
     # holland looks for plugins in holland.backup
     entry_points="""
       [holland.backup]
