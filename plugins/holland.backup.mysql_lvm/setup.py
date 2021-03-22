809344cddc0788becb6491f8d3f25094c84255e1
Steven Soulen <steven.soulen@rackspace.com>
2021-03-22 17:17:36 -0500

    bump version

--- plugins/holland.backup.mysql_lvm/setup.py
+++ plugins/holland.backup.mysql_lvm/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
-version = "1.2.4"
+version = "1.2.5"
 
 setup(
     name="holland.backup.mysql_lvm",
--- plugins/holland.backup.pgdump/setup.py
+++ plugins/holland.backup.pgdump/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = "1.2.4"
+version = "1.2.5"
 
 setup(
     name="holland.backup.pgdump",
