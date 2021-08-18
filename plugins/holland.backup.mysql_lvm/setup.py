0f59968b6ed77c3d95104ecd8ec9250d07677096
stev9171 <steven.soulen@rackspace.com>
2021-08-18 12:27:03 -0500

    Have mariadb and xtrabackups expand backupdir in pre-command

--- plugins/holland.backup.mysql_lvm/setup.py
+++ plugins/holland.backup.mysql_lvm/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import find_packages, setup
 
-version = "1.2.6"
+version = "1.2.7"
 
 setup(
     name="holland.backup.mysql_lvm",
--- plugins/holland.backup.mysqldump/setup.py
+++ plugins/holland.backup.mysqldump/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = "1.2.6"
+version = "1.2.7"
 
 setup(
     name="holland.backup.mysqldump",
