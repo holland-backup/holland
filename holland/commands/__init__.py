4325c0d7a0c9da9127a91f3c54283c0179c0e85b
Ed Velez <ed.velez@rackspace.com>
2024-04-11 15:04:01 -0500

    Silence pkg_resources deprecation warning

--- holland/commands/__init__.py
+++ holland/commands/__init__.py
@@ -2,4 +2,12 @@
 Define command plugins
 """
 
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    category=DeprecationWarning,
+    message="pkg_resources is deprecated as an API.*",
+)
+
 __import__("pkg_resources").declare_namespace(__name__)
